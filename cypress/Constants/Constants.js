c016e77a9fa213095a2ab21b82f0e457f224e4b4
ashsargsyan <ashot.sarg999@gmail.com>
2024-12-02 01:10:28 +0400

    Test Update

---
+++ cypress/Constants/Constants.js
@@ -0,0 +1,25 @@
+import {faker, Sex} from "@faker-js/faker";
+
+const constants = {
+    baseUrl: 'https://gorest.co.in/public/v2/users/',
+    token: "e862fedf4a02ac25017cd15059bb0096fc8d59135a5cf22f2aaaead2492bb52e",
+}
+
+const authorization = {
+    Authorization: 'Bearer ' + constants.token
+}
+
+const userInfo = {
+    gender: 'male',
+    name: faker.person.fullName({sex: 'male'}),
+    email: faker.internet.email(),
+    status: 'active',
+}
+
+const updatedUserInfo = {
+    name: faker.person.fullName({sex: 'male'}),
+    email: faker.internet.email(),
+    status: 'active',
+}
+
+export {constants, userInfo, updatedUserInfo, authorization}
--- cypress/e2e/Gorest Tests/Gorest API test.cy.js
+++ cypress/e2e/Gorest Tests/Gorest API test.cy.js
@@ -1,95 +1,79 @@
-import {faker} from "@faker-js/faker";
+import {constants, userInfo, updatedUserInfo, authorization} from "../../Constants/Constants";
 
-describe('User CRUD operations', () => {
-  it('Should Create, Read, Update and Delete User', () => {
-    let id;
-    const token = "e862fedf4a02ac25017cd15059bb0096fc8d59135a5cf22f2aaaead2492bb52e";
-    const baseUrl = 'https://gorest.co.in/public/v2/users/';
-    const authorization = {Authorization: 'Bearer ' + token};
-    const gender = faker.person.sexType();
-    const name = faker.person.fullName();
-    const email = faker.internet.email()
-    const status = 'active';
-    const updatedName = faker.person.fullName();
-    const updatedEmail = faker.internet.email();
-    const userInfo = {
-      "name": name,
-      "gender": gender,
-      "email": email,
-      "status": status
-    }
-    const updatedUserInfo = {
-      "name": updatedName,
-      "email": updatedEmail,
-      "status": status
-    }
+describe('User Create, Read, Update, Delete operations', () => {
+  let id;
 
+  it('Should Create User', () => {
     cy.request({
       method: 'POST',
-      url: baseUrl,
+      url: constants.baseUrl,
       headers: authorization,
       body: userInfo,
     }).then(response => {
       expect(response.status).to.eq(201)
-      expect(response.body).to.have.property('name').and.eq(name)
-      expect(response.body).to.have.property('gender').and.eq(gender)
-      expect(response.body).to.have.property('email').and.eq(email)
-      expect(response.body).to.have.property('status').and.eq(status)
+      expect(response.body).to.have.property('name').and.eq(userInfo.name)
+      expect(response.body).to.have.property('gender').and.eq(userInfo.gender)
+      expect(response.body).to.have.property('email').and.eq(userInfo.email)
+      expect(response.body).to.have.property('status').and.eq(userInfo.status)
       expect(response.body).to.have.property('id')
       id = response.body.id
     }).then(() => {
       cy.request({
         method: 'GET',
-        url: `${baseUrl}${id}`,
+        url: `${constants.baseUrl}${id}`,
         headers: authorization,
       })
     }).then(response => {
       expect(response.status).to.eq(200)
       expect(response.body.id).to.eq(id)
-      expect(response.body.name).to.eq(name)
-      expect(response.body.gender).to.eq(gender)
-      expect(response.body.email).to.eq(email)
-      expect(response.body.status).to.eq(status)
-    }).then(() => {
-      cy.request({
-        method: 'PATCH',
-        url: `${baseUrl}${id}`,
-        headers: authorization,
-        body: updatedUserInfo,
-      })
+      expect(response.body.name).to.eq(userInfo.name)
+      expect(response.body.gender).to.eq(userInfo.gender)
+      expect(response.body.email).to.eq(userInfo.email)
+      expect(response.body.status).to.eq(userInfo.status)
+    })
+  })
+
+  it('Should Update User', () => {
+    cy.request({
+      method: 'PATCH',
+      url: `${constants.baseUrl}${id}`,
+      headers: authorization,
+      body: updatedUserInfo,
     }).then(response => {
       expect(response.status).to.eq(200)
       expect(response.body.id).to.eq(id)
-      expect(response.body.name).to.eq(updatedName)
-      expect(response.body.email).to.eq(updatedEmail)
-      expect(response.body.gender).to.eq(gender)
-      expect(response.body.status).to.eq(status)
+      expect(response.body.name).to.eq(updatedUserInfo.name)
+      expect(response.body.email).to.eq(updatedUserInfo.email)
+      expect(response.body.gender).to.eq(userInfo.gender)
+      expect(response.body.status).to.eq(updatedUserInfo.status)
     }).then(() => {
       cy.request({
         method: 'GET',
-        url: `${baseUrl}${id}`,
+        url: `${constants.baseUrl}${id}`,
         headers: authorization,
       })
     }).then(response => {
       expect(response.status).to.eq(200)
       expect(response.body.id).to.eq(id)
-      expect(response.body.name).to.eq(updatedName)
-      expect(response.body.gender).to.eq(gender)
-      expect(response.body.email).to.eq(updatedEmail)
-      expect(response.body.status).to.eq(status)
-    }).then(() => {
-      cy.request({
-        method: 'DELETE',
-        url: `${baseUrl}${id}`,
-        headers: authorization,
-      })
+      expect(response.body.name).to.eq(updatedUserInfo.name)
+      expect(response.body.gender).to.eq(userInfo.gender)
+      expect(response.body.email).to.eq(updatedUserInfo.email)
+      expect(response.body.status).to.eq(updatedUserInfo.status)
+    })
+  })
+
+  it('Should Delete User ', () => {
+    cy.request({
+      method: 'DELETE',
+      url: `${constants.baseUrl}${id}`,
+      headers: authorization,
     }).then(response => {
       expect(response.status).to.eq(204)
       expect(response.body).to.be.empty;
     }).then(() => {
       cy.request({
         method: 'GET',
-        url: `${baseUrl}${id}`,
+        url: `${constants.baseUrl}${id}`,
         headers: authorization,
         failOnStatusCode: false,
       })
